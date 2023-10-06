2308aced8f627e19dd8346fb2c97f66d17456e89
harrison pepese <harrisonpepese@gmail.com>
2023-10-06 11:52:23 -0300

    feat: user module

---
+++ src/user/index.ts
@@ -0,0 +1,13 @@
+import { ChangePasswordDto } from "./dto/changePassword.dto";
+import { ChangeUserRoleDto } from "./dto/changeUserRole.dto";
+import { CreateUserDto } from "./dto/createUser.dto";
+import { EUserRole } from "./enum/userRole.enum";
+import { IUser } from "./interface/user.interface";
+
+export {
+    IUser,
+    EUserRole,
+    ChangePasswordDto,
+    ChangeUserRoleDto,
+    CreateUserDto
+}
