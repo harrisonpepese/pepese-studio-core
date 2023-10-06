950bd2b82d7c21fd23088388a73cafefeb58c49e
harrison pepese <harrisonpepese@gmail.com>
2023-10-06 10:56:10 -0300

    fix: correção de nome

--- src/pets/interface/pet.interface.ts
+++ src/pets/interface/pet.interface.ts
@@ -10,7 +10,7 @@ export interface IPet {
     level: number;
     baseAttributes: IPetAttributes;
     currentAttributes: IPetAttributes;
-    attributPoints: number;
+    attributePoints: number;
     experience: number;
     created_at: Date;
     updated_at: Date;
